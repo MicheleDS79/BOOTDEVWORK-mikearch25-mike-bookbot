74f1dc1a9748defb770b85ed987e2a7f8a0c89af
Mike Sherokee <151487504+MicheleDS79@users.noreply.github.com>
2025-03-11 22:18:39 -0400

    Used first functions to obtain text + word count .split() method

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        book_text = f.read()
+        return book_text
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    word_count = count_words(book_text)
+    print(f"{word_count} words found in this document")
+
+
+
+main()
+
